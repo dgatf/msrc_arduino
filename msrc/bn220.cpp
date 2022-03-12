76f228c4b9f5418679d71323fceb77ad863a7f94
Daniel Gorbea <danielgorbea@gmail.com>
2022-03-12 13:56:23 -0500

    Fix kmh transformation

--- msrc/bn220.cpp
+++ msrc/bn220.cpp
@@ -165,7 +165,7 @@ void Bn220::parser(uint8_t type, char *data)
         else if (type == BN220_SPD)
         {
             spd_ = atof(data);
-            spdKmh_ = spd_ * 1.852;
+            spdKmh_ = spd_ / 1.852;
         }
         else if (type == BN220_COG)
         {
